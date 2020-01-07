1e00c57b32abce75e7bbdea71871a9abb89ed1f6
Vida Wang <github@vida.wang>
2020-01-07 20:36:37 +0800

    cross step 1

--- cross.js
+++ cross.js
@@ -1,25 +1,34 @@
 let EMA = require("./ema");
 let Tools = require("./tools");
+var sha3 = require("js-sha3");
 class Cross {
-    constructor({ db, coin, interval, n1, n2, count, valve, timeGap = 0, refreshInterval = 5000, callback }) {
+    constructor({ database, coin, interval, n1, n2, count, running=true, valve, uc = "金叉", dc = "死叉", timeGap = 0, refreshInterval = 5000, callback }) {
+        if(!(database && coin && interval && n1 && n2 && count && valve)){
+            return undefined;
+        }
         this.crossArr = [];
         this.coin = coin.toUpperCase();
         this.interval = interval;
         this.n1 = n1;
+        this.uc = uc;
+        this.dc = dc;
         this.n2 = n2;
         this.count = count;
         this.timeGap = timeGap;
         this.valve = valve;
         this.callback = callback;
         this.refreshInterval = refreshInterval;
-        this.db = db;
+        this.db = database;
         this.readyA = false;
         this.readyB = false;
         this.ema1 = undefined;
         this.ema2 = undefined;
         this.resultJson = undefined;
         let me = this;
+        this.running = true;
         this.finding = false;
+        this.destroyed = false;
+        this.hash = sha3.sha3_256(coin + interval + n1 + n2 + count);
 
         this.readyCallback = function () {
             if (me.ema1 && me.ema2) {
@@ -44,6 +53,8 @@ class Cross {
                                 json.interval = me.ema1.interval;
                                 json.emaA = lastEMA1;
                                 json.emaB = lastEMA2;
+                                json.dc = me.dc;
+                                json.uc = me.uc;
                                 json.currentTime = time;
                                 json.fmtCurrentTime = Tools.Millis2Time(json.currentTime);
                                 json.time = me.ema1.emaArr[me.ema1.emaArr.length - 1].time;
@@ -51,8 +62,11 @@ class Cross {
                                 json.count = me.ema1.count;
                                 json.n1 = me.ema1.N;
                                 json.n2 = me.ema2.N;
-                                json.toString = me.jsonToString;
+                                json.valve = me.valve;
                                 json.crossType = me.jsonCrossType;
+                                json.toString = me.jsonToString;
+                                json.toSubject = me.jsonToSubject;
+                                json.toEmail = me.jsonToEamil;
 
                                 if (me.timeGap != 0) {
                                     if (time - json.time <= me.timeGap) {
@@ -84,34 +98,59 @@ class Cross {
                 });
             }
         };
+        this.start();
     }
-    jsonToString(){
-        return "current time: " + this.fmtCurrentTime + "(" + this.currentTime + ")" +
-        ", coin: " + this.coin +
+    toString(){
+        return "coin: " + this.coin +
         ", interval: " + this.interval +
         ", time: " + this.fmtTime + "(" + this.time +
-        "), EMA(" + this.n1 + "): " + this.emaA +
-        ", EMA(" + this.n2 + "): " + this.emaB +
-        ", count: " + this.count +
-        ", type: " + (this.direction ? "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") downward" : "EMA(A) cross EMA(B) upward");
+        "), N1: " + this.n1 +
+        ", N2: " + this.n2 +
+        ", count: " + this.count;
     }
-    jsonCrossType(){
-        return this.direction ? "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") downward" : "EMA(A) cross EMA(B) upward";
+    jsonToString() {
+        return "current time: " + this.fmtCurrentTime + "(" + this.currentTime + ")" +
+            ", coin: " + this.coin +
+            ", interval: " + this.interval +
+            ", time: " + this.fmtTime + "(" + this.time +
+            "), EMA(" + this.n1 + "): " + this.emaA +
+            ", EMA(" + this.n2 + "): " + this.emaB +
+            ", count: " + this.count +
+            ", type: " + this.crossType();
+    }
+    jsonToSubject() {
+        return this.coin + "-预警-" + (this.direction ? this.dc : this.uc);
+    }
+    jsonToEamil() {
+        return '币种: ' + this.coin +
+            "\n图表时间: " + this.fmtTime +
+            '\n币种: ' + this.coin +
+            '\n图表类型(分钟): ' + this.interval +
+            '\nEMA(' + this.n1 + '): ' + this.emaA +
+            '\nEMA(' + this.n2 + '): ' + this.emaB +
+            '\n预警通知时间: ' + this.fmtCurrentTime +
+            '\n预警阈值: ' + this.valve;
+    }
+    jsonCrossType() {
+        return this.direction ? "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") downward" : "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") upward";
     }
 
     notify(json) {
         let me = this;
-        if(this.db.findNotified(json, function(row){
-            if(!row){
+        if (this.db.findNotified(json, function (row) {
+            if (!row) {
                 me.db.newLog("notified. " + json.toString());
                 me.db.add2Notified(json);
                 me.resultJson = json;
                 me.callback(json);
-            }else{
+            } else {
                 me.db.newLog("existed. " + json.toString());
                 me.resultJson = json;
             }
         }));
+    }
+    changeValve(valve) {
+        this.valve = valve;
     }
 
     checkReadyA(ema) {
@@ -126,43 +165,52 @@ class Cross {
             this.readyCallback();
         }
     }
-    loadEMA(){
-        let me = this;
-        if (!this.finding) {
-            new EMA({ coin: this.coin, count: this.count, n: this.n1, interval: this.interval }, function (ema) {
-                me.checkReadyA(ema);
-            });
-            new EMA({ coin: this.coin, count: this.count, n: this.n2, interval: this.interval }, function (ema) {
-                me.checkReadyB(ema);
-            });
-        }
-    }
     changeSpeed(refreshInterval) {
         this.refreshInterval = refreshInterval;
         this.start();
     }
-    stop(){
+    stop() {
         try {
             clearInterval(this.timer);
         } catch (error) { }
+        this.running = false;
+        this.db.changeStatus(this);
     }
-    start(){
+    start() {
+        let me = this;
         try {
             clearInterval(this.timer);
         } catch (error) { }
-        this.timer = setInterval(this.loadEMA, this.refreshInterval);
+        this.timer = setInterval(function(){
+            if (!this.finding) {
+                new EMA({ coin: me.coin, count: me.count, n: me.n1, interval: me.interval }, function (ema) {
+                    me.checkReadyA(ema);
+                });
+                new EMA({ coin: me.coin, count: me.count, n: me.n2, interval: me.interval }, function (ema) {
+                    me.checkReadyB(ema);
+                });
+            }
+        }, this.refreshInterval);
+        this.running = false;
+        this.db.changeStatus(this);
+    }
+    destroy(callback){
+        this.stop();
+        this.destroyed = true;
+        this.db.deleteListener(this);
+        if(callback) callback();
     }
 }
 module.exports = Cross;
 
-const tg = 3600000;
-//unit-test
-new Cross({
-    db: undefined, coin: "btc-usdt", interval: "15", n1: 7, n2: 30, count: 384, valve: 200, refreshInterval: 1000, timeGap: tg, callback: function (json) {
-        if (!json) throw (new Error("cross 1"));
-        if (json.coin != "BTC-USDT") throw (new Error("cross 2"));
-        if (json.currentTime - json.time >= tg) throw (new Error("cross beyond time gap"));
-        //console.log(json.toString());
-        //console.log(json.crossType());
-    }
-});
+// const tg = 3600000;
+// //unit-test
+// new Cross({
+//     database: undefined, coin: "btc-usdt", interval: "15", n1: 7, n2: 30, count: 384, valve: 200, refreshInterval: 1000, timeGap: tg, callback: function (json) {
+//         if (!json) throw (new Error("cross 1"));
+//         if (json.coin != "BTC-USDT") throw (new Error("cross 2"));
+//         if (json.currentTime - json.time >= tg) throw (new Error("cross beyond time gap"));
+//         //console.log(json.toString());
+//         //console.log(json.crossType());
+//     }
+// });
--- server.js
+++ server.js
@@ -3,84 +3,286 @@ var Cross = require('./cross');
 var http = require('http');
 var Tools = require("./tools");
 var url = require("url");
+var sha3 = require("js-sha3");
 var MailgunSender = require('./mailgun-sender');
 var Sqlite3Tools = require('./sqlite3-tools');
-var dbLog = new Sqlite3Tools("log.db");
-var ms;
+
+var db = new Sqlite3Tools("log.db");
+
+var refreshTimeSpan = 5000, domainName = "", port = 0, version = "", pendingMissions = [], lastEmaA = 0, lastEmaB = 0, listeners = [], VALVE = 200, log = "";
+var fromAddress = "", arrOwners = [];
+var ms, dc, uc;
+var notifyTimeGap = 60 * 60 * 1000;
+
+//get info from file
 let fs = require("fs");
-fs.readFile("./apikey.txt",{encoding:"utf-8"},function(err,data){
-    if(err) throw err;
-    ms = new MailgunSender(data);
+fs.readFile("./info.txt", { encoding: "utf-8" }, function (err, data) {
+    if (err) Tools.GetTimeStampFromTaobao(function (time) {
+        db.newLog("err in new mailgun sender init");
+        db.close();
+        process.exit(1);
+    });
+    let json;
+    try {
+        json = JSON.parse(data);
+    } catch (error) {
+        db.newLog("err in new mailgun sender init JSON.parse");
+        db.close();
+        process.exit(1);
+    }
+
+    ms = new MailgunSender(json.apikey);
+    console.log("apikey loaded: " + json.apikey);
+    domainName = json.domain_name;
+    console.log("domain name loaded: " + json.domain_name);
+    port = parseInt(json.port);
+    console.log("port loaded: " + json.port);
+    fromAddress = json.from_address;
+    console.log("from address loaded: " + json.from_address);
+    version = json.version;
+    console.log("version loaded: " + json.version);
+    arrOwners = eval(json.arr_owners);
+    console.log("arr owners loaded: " + json.arr_owners);
+    dc = json.dc;
+    uc = json.uc;
+    init();
 });
 
-var lastEmaA = 0, lastEmaB = 0;
-var timer;
-var VALVE = 0.5;
-var notified = [];
-var log = "";
+function notify(json) {
+    let Subject = json.toSubject();
+    let Content = json.toEmail();
+    sendEmail2Owner(Subject, Content);
+    console.log(json);
+};
 
-function checkCross(Coin, Interval, N1, N2, Count, Valve) {
-    //console.log("开始执行任务");
-    Tools.GetTimeStampFromTaobao(function(startTime){
-        new EMA({ coin: Coin, count: Count, n: N1, interval: Interval }, function (ema1) {
-            new EMA({ coin: Coin, count: Count, n: N2, interval: Interval }, function (ema2) {
-                new Cross(ema1, ema2, Valve, function (cro) {
-                    let lastOne = cro[cro.length - 1];
-                    let f = true;
-                    for (let i of notified) {
-                        if (i == lastOne.time) {
-                            f = false;
-                            break;
-                        }
-                    }
-                    if (f) {
-                        if (lastEmaA != lastOne.emaA || lastEmaB != lastOne.emaB) {
-                            lastEmaA = lastOne.emaA;
-                            lastEmaB = lastOne.emaB;
-                            //console.log("需要通知");
-                            dbLog.newLog("t: " + startTime + ", ea: " + lastOne.emaA + ", eb: " + lastOne.emaB + ", s: y");
-                            notify(Coin, N1, N2, Interval, lastOne);
-                            notified.push(lastOne.time);
-                        }else{
-                            dbLog.newLog("t: " + startTime + ", ea: " + lastOne.emaA + ", eb: " + lastOne.emaB + ", s: n");
-                        }
-                    }
-                });
-            });
-        });
+function sendEmail2Owner(s, c) {
+    if (!c) c = "Nothing to show";
+    ms.Send({
+        from: fromAddress, toArr: arrOwners,
+        subject: s, content: c
+    });
+};
+
+function addListener(json) {
+    let crossObj = new Cross({
+        database: db,
+        coin: json.coin,
+        interval: json.interval,
+        n1: json.n1,
+        n2: json.n2,
+        count: json.count,
+        valve: json.valve,
+        timeGap: notifyTimeGap,
+        dc: dc,
+        uc: uc,
+        callback: function (json) {
+            notify(json);
+        }
+    });
+    json.crossObj = crossObj;
+    db.addListener(crossObj, function (success) {
+        if (success) {
+            db.newLog("new listener was added. " + json.crossObj.toString());
+            listeners.push(json);
+        } else {
+            db.newLog("tried to add listener but the listener was exist. " + json.crossObj.toString());
+        }
+    });
+};
+
+function findListener(id) {
+    for (let i of listeners) {
+        if (i.id == id) return i;
+    }
+    return undefined;
+};
+
+function removeListenerFromArr(id) {
+    for (let i = 0; i < listeners.length; i++) {
+        if (listeners[i].id == id) {
+            listeners.splice(i, 1);
+            break;
+        }
+    }
+};
+
+function deleteListener(id) {
+    let item = findListener(id);
+    if (!item) {
+        db.newLog("couldn't find timer with id");
+        cmdShowListeners();
+        return undefined;
+    }
+    item.crossObj.destroy();
+    removeListenerFromArr(id);
+}
+
+
+function startListener(id) {
+    for (let i of listeners) {
+        if (i.id == id) {
+            i.crossObj.start();
+            break;
+        }
+    }
+}
+
+function stopListener(id) {
+    for (let i of listeners) {
+        if (i.id == id) {
+            i.crossObj.stop();
+            break;
+        }
+    }
+}
+function stopAllListeners() {
+    for (let i of listeners) i.crossObj.stop();
+}
+function startAllListeners() {
+    for (let i of listeners) i.crossObj.start();
+}
+
+function cmdAddListener(i) {
+    addListener({ coin: i.coin, interval: i.interval, n1: i.n1, n2: i.n2, count: i.count, valve: i.valve }, function (id) {
+        if (id != -1) sendEmail2Owner("添加成功", "监听器已成功添加, 新添加的监听器id为：" + id);
+        else sendEmail2Owner("添加失败", "监听器已存在");
+    });
+}
+function cmdShowListeners() {
+    let res = "";
+    for (let i of listeners) {
+        res += i.crossObj.toString() + "\r\n";
+    }
+    sendEmail2Owner("监听器列表", res);
+}
+function cmdDeleteListeners(i) {
+    deleteListener(i.id);
+}
+function cmdModifyRefreshTimeSpan(i) {
+    let j = refreshTimeSpan;
+    refreshTimeSpan = i.timespan;
+    stopAllListeners();
+    startAllListeners();
+
+    sendEmail2Owner("修改刷新时间间隔成功", "刷新时间间隔由" + j + "更改为" + i.timespan);
+}
+function cmdModifyValve(i) {
+    let j = VALVE;
+    for (let item of listeners) {
+        item.changeValve(i.valve);
+    }
+    sendEmail2Owner("修改阈值成功", "阈值由" + j + "更改为" + i.valve);
+}
+function cmdShowLogs(i) {
+    if (!i) {
+        i = {};
+    }
+    db.getLatestLogs(i.count, true, function (res) {
+        sendEmail2Owner("日志", res);
+    });
+}
+function cmdStopListener(i) {
+    stopListener(i.id);
+    sendEmail2Owner("监听器已停止", "ID为" + i.id + "的监听器已停止运作");
+}
+function cmdStartListener(i) {
+    startListener(i.id);
+    sendEmail2Owner("监听器已启动", "ID为" + i.id + "的监听器已开始运作");
+}
+function removeFromPending(hash) {
+    for (let j = 0; j < pendingMissions.length; j++) {
+        if (pendingMissions[j].hash == hash) {
+            pendingMissions.splice(j, 1);
+            break;
+        }
+    }
+}
+function cmdClearLogs() {
+    db.clearLogs();
+    cmdShowLogs();
+}
+
+function init() {
+    db.fetchListeners(function (arr) {
+        for (let i of arr) {
+            listeners.push(new Cross({
+                database: db,
+                coin: i.coin,
+                interval: i.interval,
+                n1: i.n1,
+                n2: i.n2,
+                count: i.count,
+                valve: i.valve,
+                running: i.status ? true : false,
+                timeGap: notifyTimeGap,
+                dc: dc,
+                uc: uc,
+                callback: function (json) {
+                    notify(json);
+                }
+            }));
+        }
     });
 
-    
-};
+    http.createServer(function (req, res) {
+        res.writeHead(200, { "Content-type": "text/html;charset=UTF-8" });
+        let query = url.parse(req.url, true).query;
+        if (req.url.indexOf("command") >= 0) {
+            let json = {};
+            json.hash = sha3.sha3_512(new Date().getTime().toString() + query.command);
+            json.query = query;
+            json.timer = setTimeout(function () {
+                removeFromPending(json.hash);
+            }, 600000);
+            pendingMissions.push(json);
 
-function notify(coin, n1, n2, interval, obj) {
-    let Subject = coin + "预警-" + obj.fmtTime;
-    let Content = "时间：" + obj.fmtTime +
-        '\n币种：' + coin +
-        '\nInterval: ' + interval.toString() +
-        '\nEMA(' + n1.toString() + ')：' + obj.emaA.toString() +
-        '\nEMA(' + n2.toString() + ')：' + obj.emaB.toString() +
-        '\n预警阈值(美元)：' + VALVE.toString();
+            sendEmail2Owner("未执行的操作", "点击以下链接已继续执行指令：\nhttp://" + domainName + ":" + port + "/pending?hash=" + json.hash);
+            console.log("未执行的操作", "点击以下链接已继续执行指令：\nhttp://" + domainName + ":" + port + "/pending?hash=" + json.hash);
+            db.newLog("new command arived: " + query.command);
+            console.log("new request arived");
+            res.end("success");
+        } else if (req.url.indexOf("pending") >= 0) {
+            let i = undefined;
+            for (let j of pendingMissions) {
+                if (j.hash == query.hash) {
+                    i = j.query;
+                    clearTimeout(j.timer);
+                    removeFromPending(j.hash);
+                    break;
+                }
+            }
 
-    ms.Send({ from: "test@sandbox62269d57285c40e0956479f8d08e4ae2.mailgun.org", toArr: ['872889318@qq.com'], subject: Subject, content: Content });
-};
+            if (i) {
+                if (i.command == "add_listener") {
+                    cmdAddListener(i);
+                } else if (i.command == "show_listeners") {
+                    cmdShowListeners();
+                } else if (i.command == "delete_listener") {
+                    cmdDeleteListeners(i);
+                } else if (i.command == "modify_refresh_timespan") {
+                    cmdModifyRefreshTimeSpan(i);
+                } else if (i.command == "modify_valve") {
+                    cmdModifyValve(i);
+                } else if (i.command == "show_logs") {
+                    cmdShowLogs(i);
+                } else if (i.command == "stop_listener") {
+                    cmdStopListener(i);
+                } else if (i.command == "start_listener") {
+                    cmdStartListener(i);
+                } else if (i.command == "clear_logs") {
+                    cmdClearLogs();
+                }
+                res.end("success");
+            } else {
+                res.end("unknown error");
+            }
+        }
+    }).listen(port, domainName);
 
-timer = setInterval(function () {
-    checkCross("btc-usdt", 15, 7, 30, 383, VALVE);
-}, 5000);
+    console.log("server started");
 
-
-let server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-type": "text/html;charset=UTF-8" });
-    let query = url.parse(req.url, true).query;
-    if (req.url.indexOf("cross") >= 0) {
-        ms.Send({ from: "test@sandbox62269d57285c40e0956479f8d08e4ae2.mailgun.org", toArr: ['872889318@qq.com'], 
-        subject: "更改阈值通知", content: "阈值由" + VALVE.toString() + "更改为" + query.valve });
-        VALVE = parseFloat(query.valve);
-        res.end("success");
-    }else if(req.url.indexOf("showlog")){
-
-    }
-});
-
-server.listen(2424, "139.180.141.189");
+    Tools.GetTimeStampFromTaobao(function (st) {
+        sendEmail2Owner("服务已启动", "时间: " + Tools.Millis2Time(st) + "(" + st + ")");
+        db.newLog("server started at: " + Tools.Millis2Time(st) + "(" + st + ")" + ", version: " + version)
+    });
+}
