260ceb4255544eefd5ae09277865e5b4ab5b8d28
Vida Wang <github@vida.wang>
2020-01-07 12:05:10 +0800

    change cross callback

--- cross.js
+++ cross.js
@@ -1,82 +1,163 @@
 let EMA = require("./ema");
+let Tools = require("./tools");
 class Cross {
-    constructor(ema1, ema2, valve, fun) {
+    constructor({ db, coin, interval, n1, n2, count, valve, timeGap = 0, refreshInterval = 5000, callback }) {
         this.crossArr = [];
+        this.coin = coin.toUpperCase();
+        this.interval = interval;
+        this.n1 = n1;
+        this.n2 = n2;
+        this.count = count;
+        this.timeGap = timeGap;
         this.valve = valve;
-        this.fun = fun;
-        this.findCross(ema1, ema2);
-    }
+        this.callback = callback;
+        this.refreshInterval = refreshInterval;
+        this.db = db;
+        this.readyA = false;
+        this.readyB = false;
+        this.ema1 = undefined;
+        this.ema2 = undefined;
+        this.resultJson = undefined;
+        let me = this;
+        this.finding = false;
+        this.load();
 
-    findEqual(ema1, ema2) {
-        let direction = ema1.emaArr[1].ema > ema2.emaArr[1].ema ? true : false;
+        this.readyCallback = function () {
+            if (me.ema1 && me.ema2) {
+                me.finding = true;
+                Tools.GetTimeStampFromTaobao(function (time) {
+                    if (me.ema1 && me.ema2) {
+                        let json = {};
+                        if (me.ema1.emaArr.length >= 2) {
+                            let lastEMA1 = me.ema1.emaArr[me.ema1.emaArr.length - 1].ema;
+                            let lastEMA2 = me.ema2.emaArr[me.ema2.emaArr.length - 1].ema;
 
-        for (let i = 0; i < ema1.emaArr.length; i++) {
-            let json = {};
-            if (i > 1) {
-                if (Math.abs(ema1.emaArr[i].ema - ema2.emaArr[i].ema) <= this.valve) {
-                    if (direction) {
-                        json.direction = '1dc2';
-                        direction = !direction;
-                    } else {
-                        json.direction = '1uc2';
-                        direction = !direction;
+                            if (Math.abs(lastEMA1 - lastEMA2) <= me.valve) {
+                                let last2EMA1 = me.ema1.emaArr[me.ema1.emaArr.length - 2].ema;
+                                let last2EMA2 = me.ema2.emaArr[me.ema2.emaArr.length - 2].ema;
+
+                                if (last2EMA1 >= last2EMA2) {
+                                    json.direction = true;
+                                } else {
+                                    json.direction = false;
+                                }
+                                json.coin = me.ema1.coin;
+                                json.interval = me.ema1.interval;
+                                json.emaA = lastEMA1;
+                                json.emaB = lastEMA2;
+                                json.currentTime = time;
+                                json.fmtCurrentTime = Tools.Millis2Time(json.currentTime);
+                                json.time = me.ema1.emaArr[me.ema1.emaArr.length - 1].time;
+                                json.fmtTime = me.ema1.emaArr[me.ema1.emaArr.length - 1].fmtTime;
+                                json.count = me.ema1.count;
+                                json.n1 = me.ema1.N;
+                                json.n2 = me.ema2.N;
+                                json.toString = me.jsonToString;
+                                json.crossType = me.jsonCrossType;
+
+                                if (me.timeGap != 0) {
+                                    if (time - json.time <= me.timeGap) {
+                                        if (me.resultJson) {
+                                            if (json.emaA != me.resultJson.emaA || json.emaB != me.resultJson.emaB) {
+                                                me.notify(json);
+                                            }
+                                        } else {
+                                            me.notify(json);
+                                        }
+                                    } else {
+                                        me.db.newLog("beyond time gap. " + json.toString());
+                                    }
+                                } else {
+                                    if (me.resultJson) {
+                                        if (json.emaA != me.resultJson.emaA || json.emaB != me.resultJson.emaB) {
+                                            me.notify(json);
+                                        }
+                                    } else {
+                                        me.notify(json);
+                                    }
+                                }
+                            }
+                        }
+                        me.ema1 = undefined;
+                        me.ema2 = undefined;
+                        me.finding = false;
                     }
-                    json.time = ema1.emaArr[i].time;
-                    json.fmtTime = ema1.emaArr[i].fmtTime;
-                    json.emaA = ema1.emaArr[i].ema;
-                    json.emaB = ema2.emaArr[i].ema;
-                    this.crossArr.push(json);
-                }
+                });
             }
-        }
-        this.fun(this.crossArr);
+        };
     }
-    findCross(ema1, ema2) {
-        let itv1 = ema1.interval;
-        let itv2 = ema2.interval;
-
-        itv1 = this.convertInterval(itv1) * 60 * 1000;
-        itv2 = this.convertInterval(itv2) * 60 * 1000;
-
-        if (itv1 == itv2) {
-            this.findEqual(ema1, ema2);
-        } else {
-            this.findNEqual(ema1, ema2);
-        }
-
+    jsonToString(){
+        return "current time: " + this.fmtCurrentTime + "(" + this.currentTime + ")" +
+        ", coin: " + this.coin +
+        ", interval: " + this.interval +
+        ", time: " + this.fmtTime + "(" + this.time +
+        "), EMA(" + this.n1 + "): " + this.emaA +
+        ", EMA(" + this.n2 + "): " + this.emaB +
+        ", count: " + this.count +
+        ", type: " + (this.direction ? "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") downward" : "EMA(A) cross EMA(B) upward");
+    }
+    jsonCrossType(){
+        return this.direction ? "EMA(" + this.n1 + ") cross EMA(" + this.n2 + ") downward" : "EMA(A) cross EMA(B) upward";
     }
 
-    convertInterval(interval) {
-        if (interval == "1d") { interval = 1440; }
-        else if (interval == "1w") { interval = 12390; }
-        else if (interval == "1m") { interval = 371700; }
-        return interval;
+    notify(json) {
+        let me = this;
+        if(this.db.findNotified(json, function(row){
+            if(!row){
+                me.db.newLog("notified. " + json.toString());
+                me.db.add2Notified(json);
+                me.resultJson = json;
+                me.callback(json);
+            }else{
+                me.db.newLog("existed. " + json.toString());
+                me.resultJson = json;
+            }
+        }));
+    }
+
+    checkReadyA(ema) {
+        if (!this.finding) {
+            this.ema1 = ema;
+            this.readyCallback();
+        }
+    }
+    checkReadyB(ema) {
+        if (!this.finding) {
+            this.ema2 = ema;
+            this.readyCallback();
+        }
+    }
+    load() {
+        try {
+            clearInterval(this.timer);
+        } catch (error) { }
+        let me = this;
+        this.timer = setInterval(function () {
+            if (!me.finding) {
+                new EMA({ coin: me.coin, count: me.count, n: me.n1, interval: me.interval }, function (ema) {
+                    me.checkReadyA(ema);
+                });
+                new EMA({ coin: me.coin, count: me.count, n: me.n2, interval: me.interval }, function (ema) {
+                    me.checkReadyB(ema);
+                });
+            }
+        }, this.refreshInterval);
+    }
+    changeSpeed(refreshInterval) {
+        this.refreshInterval = refreshInterval;
+        this.load();
     }
 }
-
 module.exports = Cross;
 
-// new EMA({ coin: "btc-usdt", count: 383, n: 9, interval: "1d" }, function (ema1) {
-//     new EMA({ coin: "btc-usdt", count: 383, n: 26, interval: "1d" }, function (ema2) {
-//         let b = new Cross(ema1, ema2);
-//     });
-// });
-
-
-// new EMA({coin:"btc-usdt", count: 383, n: 9, interval: "1d" },function(ema1){
-//     let c= "";
-//     for(let i of ema1.emaArr){
-//         c += i.ema.toString() + "\r\n";
-//     }
-//     let fs = require("fs");
-//     fs.writeFile("./1.txt",c,function(){});
-
-// });
-// new EMA({coin:"btc-usdt", count: 383, n: 26, interval: "1d" },function(ema2){
-//     let a= "";
-//     for(let j of ema2.emaArr){
-//         a += j.ema.toString() + "\r\n";
-//     }
-//     let fs1 = require("fs");
-//     fs1.writeFile("./2.txt",a,function(){});
-// });
+const tg = 3600000;
+//unit-test
+new Cross({
+    db: undefined, coin: "btc-usdt", interval: "15", n1: 7, n2: 30, count: 384, valve: 200, refreshInterval: 1000, timeGap: tg, callback: function (json) {
+        if (!json) throw (new Error("cross 1"));
+        if (json.coin != "BTC-USDT") throw (new Error("cross 2"));
+        if (json.currentTime - json.time >= tg) throw (new Error("cross beyond time gap"));
+        //console.log(json.toString());
+        //console.log(json.crossType());
+    }
+});
--- sqlite3-tools.js
+++ sqlite3-tools.js
@@ -1,49 +1,135 @@
 var sqlite3 = require('sqlite3');
 var Tools = require("./tools");
-class Sqlite3Tools{
-    constructor({filename}){
+var sha3 = require("js-sha3");
+class Sqlite3Tools {
+    constructor(filename) {
         this.db = new sqlite3.Database(filename);
     }
-    exe(sql){
+    exe(sql) {
         let er;
-        this.db.run(sql,function(err){
+        this.db.run(sql, function (err) {
             er = err;
         });
         return er;
     }
-    getResult({sql,callback}){
-        this.db.each(sql,callback);
+    clearLogs() {
+        this.db.run("delete from log where 1=1");
     }
-    newLog(log){
-        Tools.GetTimeStampFromTaobao(function(time){
-            this.db.run("insert into log(id,time,value,extra) values(null," + time + "," + log + ",''");
+    getResult({ sql, callback }) {
+        this.db.each(sql, callback);
+    }
+    newLog(log) {
+        let me = this;
+
+        Tools.GetTimeStampFromTaobao(function (time) {
+            me.db.get("select * from log where value ='" + log + "'", function (err, row) {
+                if (row) {
+                    me.db.run("update log set times = (select times from log where id =  " + row.id + ") + 1 where id = " + row.id + "");
+                    me.db.run("update log set last_update = " + time + " where id = " + row.id + " ")
+                } else {
+                    me.db.run("insert into log(id,time,value,last_update,times) values(null,'" + time + "','" + log + "','" + time + "',0)");
+                }
+            });
         });
     }
-    getLatestLogs({count=10,returnString=false}){
+    getLatestLogs(count, returnString, callback) {
         let res, sql;
 
-        if(returnString) res = "";
+        if (returnString) res = "";
         else res = [];
 
-        if(!count) sql = "select * from log order by time desc";
-        else sql = "select * from log order by time desc limit " + count.toString();
-       
-        this.db.each(sql, function(err,row){
-            if(err) return undefined;
-            if(!returnString){
+        if (!count) sql = "select * from log order by time desc";
+        else{
+            if(parseInt(count) == 0){
+                sql = "select * from log order by time desc";
+            }else{
+                sql = "select * from log order by time desc limit " + count.toString();
+            }
+        } 
+        this.db.each(sql, function (err, row) {
+            if (err) return undefined;
+            if (!returnString) {
                 let json = {};
                 json.fmtTime = Tools.Millis2Time(row.time);
                 json.log = row.value;
                 json.extra = row.extra;
                 res.push(json);
-
-            }else{
-                res += "Time: " + Tools.Millis2Time(row.time) + ", Log: " + row.log + (row.extra.length == 0 ? "" : (", Extra Info: " + row.extra)) + "\r\n";
-            } 
+            } else {
+                res += "id: " + row.id + "\r\n" +
+                    "time: " + Tools.Millis2Time(row.time) + "(" + row.time + ")" + "\r\n" +
+                    "log: " + row.value + "\r\n" +
+                    "last_update: " + Tools.Millis2Time(row.last_update) + "(" + row.last_update + ")" + "\r\n" +
+                    "times: " + row.times + "\r\n\r\n";
+            }
+        }, function (err, count) {
+            callback(res);
         });
-        return res;
     }
-    close(){
+    addListener(coin, interval, n1, n2, count, callback) {
+        if (!(coin && interval && n1 && n2 && count)) {
+            //console.log(coin);
+            //console.log(interval);
+            //console.log(n1);
+            //console.log(n2);
+            //console.log(count);
+            this.newLog("argumens missing in addListener");
+            return undefined;
+        }
+        let me = this;
+        let hash = sha3.sha3_256(coin + interval + n1.toString() + n2.toString() + count.toString());
+        //console.log("6.1");
+        //console.log(sql);
+        this.db.get("select * from listeners where hash='" + hash + "'", function (err, row) {
+            //console.log("6.2");
+            if (err) {
+                me.newLog("Error in this.db.get select * from listeners where hash = xxx, " + err.message);
+                return undefined;
+            }
+            //console.log("6.3");
+            if (!row) {
+                //console.log(sl);
+                me.db.run("insert into listeners(id,coin,interval,n1,n2,x_count,status,hash) values(null,'" + coin + "','" + interval + "'," + n1 + "," + n2 + "," + count + ",0,'" + hash + "')");
+                callback(0);
+            } else {
+                callback(row.id);
+            }
+        });
+    }
+
+    deleteListener(id, callback) {
+        this.db.run("delete from listeners where id = " + id, function (err) {
+            callback();
+        });
+    }
+
+    fetchListeners(callback) {
+        let arr = [];
+        this.db.each("select * from listeners", function (err, row) {
+            let json = {};
+            json.id = row.id;
+            json.coin = row.coin;
+            json.interval = row.interval;
+            json.n1 = parseInt(row.n1);
+            json.n2 = parseInt(row.n2);
+            json.count = parseInt(row.x_count);
+            arr.push(json);
+        }, function (err, count) {
+            callback(arr);
+        });
+    }
+
+    add2Notified(json) {
+        let hash = sha3.sha3_256(json.toString());
+        this.db.run("insert into notified(id, time, hash) values(null, '" + json.time + "','" + hash + "')");
+    }
+    findNotified(json, callback) {
+        let hash = sha3.sha3_256(json.toString());
+        this.db.get("select * from notified where hash='" + hash + "'", function (err, row) {
+            if (row) callback(row);
+            else callback(undefined);
+        });
+    }
+    close() {
         this.db.close();
     }
 }
